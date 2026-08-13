0b7971e330ebb7116d24c2ee31276d8daf2901e5
Mohit Anuragi <anuragimohit468@gmail.com>
2026-08-14 02:12:58 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'My-Portfolio'
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,
+  base: './',
 })
